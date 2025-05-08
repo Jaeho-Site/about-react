ee7b490ef22d5cedead6811039ea6eca9d692b1b
Jaeho <sjaeho348@gmail.com>
2025-05-09 01:26:45 +0900

    feat : vite start

--- ov/ES6/c.js
+++ ov/ES6/c.js
@@ -2,8 +2,13 @@
 //     return 'Data loaded';
 // }
 
-function fetchData() {
-	return Promise.resolve('Data loaded');
+// function fetchData() {
+// 	return Promise.resolve('Data loaded');
+// }
+
+// fetchData().then(console.log);
+function printAlert() {
+	alert('이 함수는 브라우저 환경에서만 실행됩니다.');
 }
 
-fetchData().then(console.log);
+printAlert();
